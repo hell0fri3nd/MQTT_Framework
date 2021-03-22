980989fbe45fa1e40ad416b0b9fad5e92f4fecb6
cristiano <clucian@unibz.it>
2021-03-22 12:15:21 +0100

    Added host discovery function + ports scanning

--- framework/src/mixins/scan.py
+++ framework/src/mixins/scan.py
@@ -1,9 +1,12 @@
+import scapy
 from cmd2 import with_argparser, with_category
-import argparse
-
 from prettytable import PrettyTable
-from scapy.layers.l2 import ARP, Ether
-from scapy.sendrecv import srp
+import argparse
+import nmap
+import ipaddress
+import netifaces
+import os
+import scapy.all as scapy
 
 from framework.src.interfaces import InterfaceMixin
 
@@ -21,11 +24,27 @@ class NetworkScannerMixin(InterfaceMixin):
 
     @with_category(InterfaceMixin.CMD_CAT_BROKER_OP)
     @with_argparser(scans_parser)
-    def do_net_scan(self, args):
+    def do_scan(self, args):
         if args.cached:
             self.handle_cache()
         else:
-            self.run_arp_scan()
+            # Gets client net specs
+            netdata = self.get_client_netdata()
+            # Resolving ipaddress in CIDR notation
+            hosts = ipaddress.ip_network(
+                netdata[0] + '/' +
+                netdata[1], strict=False)
+            print(hosts)
+            # hlist = self.get_host_list(netdata[0], netdata[1])
+            # ARP requests to find up hosts
+            hlist = self.resolve_up_hosts(str(hosts))
+            # Scanning each hosts ports
+            self.print_info("Port scanning started")
+            for elem in hlist:
+                self.host_scan(elem)
+
+            self.print_ok("Port scanning executed")
+            print(self.clients)
 
     def show_clients(self):
 
@@ -40,24 +59,136 @@ class NetworkScannerMixin(InterfaceMixin):
 
         self.ppaged(msg=str(scans_table))
 
-    def run_arp_scan(self):
-        # TODO: Add retrival from file
+    def host_scan(self, host):
+        # TODO: Add saving result from file
+        try:
+            nm = nmap.PortScanner()
+            arg = '-sV -p T:1883,443,8883,80'
+            res = nm.scan(hosts=str(host), arguments=arg)
+            print(res)
+            active_p = []
+            port_list = res['scan'][str(host)]['tcp']
+            for key in port_list:
+                if port_list[key] != 'closed':
+                    print(port_list[key])
+                    active_p.append({key: port_list[key]})
+            if len(active_p) != 0:
+                self.clients.append(res)
+            """
+            for sent, received in result:
+                # for each response, append ip and mac address to `clients` list
+                self.clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+            """
+            # self.show_clients()
 
-        target_ip = "192.168.1.1/24"  # IP Address for the destination
-        arp = ARP(pdst=target_ip)  # create ARP packet
-        # create the Ether broadcast packet
-        ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
-        packet = ether / arp  # stack them
-
-        result = srp(packet, timeout=5, verbose=True)[
-            0]  # result is list of pairs with format (sent_packet, received_packet)
-
-        for sent, received in result:
-            # for each response, append ip and mac address to `clients` list
-            self.clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-
-        self.show_clients()
+        except Exception as e:
+            self.print_error("host_scan error: " + e.__str__())
 
     def handle_cache(self):
         # TODO: Add retrival from file
         pass
+
+    def get_client_netdata(self):
+        try:
+
+            # Checks interfaces based on os (netifaces returns interfaces ids on windows)
+            if os.name == 'nt':
+                list = scapy.get_windows_if_list()
+                self.win_show_ifaces(list)
+            else:
+                list = netifaces.interfaces()
+                self.gen_show_ifaces(list)
+
+            while True:
+                iface_num = input("[!] Choose the device number: ")
+                try:
+                    var = list[int(iface_num)]
+                    break
+                except Exception as e:
+                    self.print_error("Choose an existing number")
+
+            self.print_info(f"Retrieving IP Address & Netmask")
+            if os.name == 'nt':
+                iface = list[int(iface_num)]['guid']
+            else:
+                iface = list[int(iface_num)]
+
+            addrs = netifaces.ifaddresses(iface)
+
+            ip_address = addrs[netifaces.AF_INET][0]['addr']
+            netmask = addrs[netifaces.AF_INET][0]['netmask']
+
+            self.print_ok(f"Network data found\n IP: {ip_address}\n Netmask: {netmask}")
+
+            return [ip_address, netmask]
+        except Exception as e:
+            self.print_error(f"get_client_netdata: " + e.__str__())
+
+    def get_host_list(self, local_ip, netmask):
+        try:
+            self.print_info(f"Computing possible")
+
+            # Generating CIDR notation
+            cidr = ipaddress.ip_network(local_ip + '/' + netmask, strict=False)
+            net = ipaddress.ip_interface(cidr).network
+            host_list = []
+            for x in net:
+                host_list.append(x)
+
+            self.print_ok(f"Finished computing")
+            return host_list
+        except Exception as e:
+            self.print_error(f"get_lan_list: " + e.__str__())
+
+    def resolve_up_hosts(self, ip):
+        self.print_info(f'Resolving up hosts')
+        arp_req_frame = scapy.ARP(pdst=ip)
+
+        broadcast_ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+        broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
+
+        answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout=1, verbose=False)[0]
+        print(answered_list)
+        result = []
+        for i in range(0, len(answered_list)):
+            result.append(answered_list[i][1].psrc)
+
+        self.print_info(f'Up hosts resolved')
+        return result
+
+    def win_show_ifaces(self, ifaces):
+        try:
+
+            scans_table = PrettyTable(field_names=[
+                '#', 'Name', 'IP Address', 'MAC Address', 'Description'
+            ])
+
+            self.print_ok(f"Network interfaces on your device:")
+
+            i = 0
+            for iface in ifaces:
+                scans_table.add_row([i, iface['name'], iface['ips'][1], iface['mac'], iface['description']])
+                i += 1
+
+            self.ppaged(msg=str(scans_table))
+        except Exception as e:
+            self.print_error(f"win_show_ifaces: " + e.__str__())
+
+    def gen_show_ifaces(self, ifaces):
+        try:
+
+            scans_table = PrettyTable(field_names=[
+                '#', 'Identifiers'
+            ])
+
+            self.print_ok(f"Network interfaces on your device:")
+
+            i = 0
+            for iface in ifaces:
+                scans_table.add_row([i, iface])
+                i += 1
+
+            self.ppaged(msg=str(scans_table))
+        except Exception as e:
+            self.print_error(f"win_show_ifaces: " + e.__str__())
