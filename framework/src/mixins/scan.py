6ad4dc4d06cc58ffd570af519b7110df8199f3a8
cristiano <clucian@unibz.it>
2021-03-31 17:17:46 +0200

    Added nmap parameters + display os table + target local scan

--- framework/src/mixins/scan.py
+++ framework/src/mixins/scan.py
@@ -30,6 +30,15 @@ class NetworkScannerMixin(InterfaceMixin):
                               action="store_true")
     scans_parser.add_argument('--verbose', help='increase output verbosity',
                               action="store_true")
+    scans_parser.add_argument('-os', '--os_scan', help='display os details of targets',
+                              action="store_true")
+    scans_parser.add_argument('-a', '--auto',
+                              help='automatic scanning of the local network, the framework will find automatically '
+                                   'the possible hosts running mqtt and display the results',
+                              action="store_true")
+    scans_parser.add_argument('-t', '--target',
+                              help='run a scan on the specified target, one host\' ip address either a list in cidr '
+                                   'notation')
 
     @with_category(InterfaceMixin.CMD_CAT_BROKER_OP)
     @with_argparser(scans_parser)
@@ -39,7 +48,20 @@ class NetworkScannerMixin(InterfaceMixin):
 
         if args.cached:
             self.handle_cache()
-        else:
+        elif args.target is not None:
+            target = args.target
+            self.print_ok("Started scanning " + target)
+            # Showing waiting animation
+            set_done(False)
+            if not args.verbose:
+                t = threading.Thread(target=waiting_animation)
+                t.start()
+            self.host_scan(target)
+            set_done(True)
+            print('\n')
+            # Show target details
+            self.show_clients(self.hosts)
+        elif args.auto:
             # Gets client net specs
             netdata = self.get_client_netdata()
             # Stops execution at user's request
@@ -59,8 +81,9 @@ class NetworkScannerMixin(InterfaceMixin):
             self.print_info("Port scanning started")
             # Showing waiting animation
             set_done(False)
-            t = threading.Thread(target=waiting_animation)
-            t.start()
+            if not args.verbose:
+                t = threading.Thread(target=waiting_animation)
+                t.start()
             for elem in hlist:
                 self.host_scan(elem)
             set_done(True)
@@ -68,6 +91,7 @@ class NetworkScannerMixin(InterfaceMixin):
             self.print_ok("Port scanning executed")
 
             # Save hosts data
+            # TODO: Filter data
             for h in self.hosts:
                 self.db.insert(h)
 
@@ -82,7 +106,7 @@ class NetworkScannerMixin(InterfaceMixin):
             self.show_hosts_table(hosts)
 
             while interacting:
-                self.print_question('Choose host number to investigate its ports, type \\q to quit')
+                self.print_question('Choose host number to investigate its details, type \\q to quit')
                 # Select the wanted host
                 while True:
                     ans = input(f"Input: ")
@@ -98,8 +122,10 @@ class NetworkScannerMixin(InterfaceMixin):
                 # Cheking if users wants to quit
                 if not interacting:
                     break
-                # Show port tables
+                # Show port and os tables
                 self.show_ports_table(chosen_h)
+                if self.args.os_scan:
+                    self.show_os_table(chosen_h)
                 # Setting target
                 self.print_question(f"Type \\y if you want to add the host to the target list")
                 ans = input(f"Input: ")
@@ -119,7 +145,7 @@ class NetworkScannerMixin(InterfaceMixin):
             ])
 
             # print hosts
-            self.print_ok("Available devices in the network:")
+            self.print_ok("Available devices in the network with at least one open port:")
 
             i = 0
             for client in hosts:
@@ -130,7 +156,12 @@ class NetworkScannerMixin(InterfaceMixin):
                     ipaddr.append(key)
 
                 inner_dict = client['scan'][str(ipaddr[0])]
-                open_ports = [p for p in inner_dict['tcp']]
+                ports = inner_dict['tcp']
+                open_ports = []
+                # shows open ports only
+                for p in ports:
+                    if ports[p]['state'] != 'closed':
+                        open_ports.append(p)
                 try:
                     mac_addr = inner_dict['addresses']['mac']
                 except Exception:
@@ -153,7 +184,7 @@ class NetworkScannerMixin(InterfaceMixin):
     def show_ports_table(self, host):
         try:
             port_table = PrettyTable(field_names=[
-                'Port number', 'State', 'Reason', 'Name', 'Product', 'Version', 'Extrainfo',
+                'Port number', 'State', 'Reason', 'Name', 'Product', 'Version', 'Extrainfo', 'Scripts'
             ])
 
             self.print_ok("Ports for the selected host:")
@@ -166,6 +197,11 @@ class NetworkScannerMixin(InterfaceMixin):
             ports = host['scan'][str(ipaddr[0])]['tcp']
 
             for key in ports:
+                try:
+                    scripts = ports[key]['script']
+                except Exception as e:
+                    scripts = ''
+
                 port_table.add_row([
                     key,
                     ports[key]['state'],
@@ -173,7 +209,49 @@ class NetworkScannerMixin(InterfaceMixin):
                     ports[key]['name'],
                     ports[key]['product'],
                     ports[key]['version'],
-                    ports[key]['extrainfo']
+                    ports[key]['extrainfo'],
+                    scripts
+                ])
+
+            self.ppaged(msg=str(port_table))
+        except Exception as e:
+            self.print_error("show_ports_table error: " + e.__str__())
+
+    def show_os_table(self, host):
+        try:
+            port_table = PrettyTable(field_names=[
+                'Name', 'Accuracy', 'Type list', 'Cpe list'
+            ])
+
+            self.print_ok("OS details:")
+            ipaddr = []
+
+            # implementing a list, an host can have multiple ips
+            for key in host['scan']:
+                ipaddr.append(key)
+
+            os_array = host['scan'][str(ipaddr[0])]['osmatch']
+
+            for elem in os_array:
+                try:
+                    type_list = []
+                    for obj in elem['osclass']:
+                        type_list.append(obj['type'])
+                except Exception as e:
+                    type_list = 'Error: ' + str(e)
+
+                try:
+                    cpe_list = []
+                    for obj in elem['osclass']:
+                        cpe_list.append(obj['cpe'])
+                except Exception as e:
+                    cpe_list = 'Error: ' + str(e)
+
+                port_table.add_row([
+                    elem['name'],
+                    elem['accuracy'],
+                    type_list,
+                    cpe_list
                 ])
 
             self.ppaged(msg=str(port_table))
@@ -181,9 +259,11 @@ class NetworkScannerMixin(InterfaceMixin):
             self.print_error("show_ports_table error: " + e.__str__())
 
     def host_scan(self, host):
+        # TODO: Add error handling for not valid host
         try:
+            self.print_verbose(str(host), self.args)
             nm = nmap.PortScanner()
-            arg = '-sV -p T:1883,8883'
+            arg = '-sV -A -p T:1883,8883 --reason'
             res = nm.scan(hosts=str(host), arguments=arg)
             self.print_verbose(res, self.args)
             active_p = []
@@ -191,8 +271,9 @@ class NetworkScannerMixin(InterfaceMixin):
             # Excludes closed ports
             for key in port_list:
                 self.print_verbose(str(key) + ': ' + str(port_list[key]), self.args)
-                if port_list[key] != 'closed':
+                if port_list[key]['state'] != 'closed':
                     active_p.append({key: port_list[key]})
+            self.print_verbose(str(len(active_p)) + ' - ' + str(active_p), self.args)
             if len(active_p) != 0:
                 self.hosts.append(res)
 
@@ -249,13 +330,13 @@ class NetworkScannerMixin(InterfaceMixin):
 
         broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
 
-        answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout=1, verbose=False)[0]
+        answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout=10, verbose=False)[0]
         self.print_verbose(answered_list, self.args)
         result = []
         for i in range(0, len(answered_list)):
             result.append(answered_list[i][1].psrc)
 
-        self.print_info(f'Up hosts resolved')
+        self.print_info(f'Up hosts resolved\n Number of up hosts: {len(result)}')
         return result
 
     def win_show_ifaces(self, ifaces):
